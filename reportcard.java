95a9e036f3799b0cb3972df3066a2b5f72aa655d
HacerKubra <hacerkubra@users.noreply.github.com>
2018-04-03 22:36:02 +0300

    Add files via upload

---
+++ reportcard.java
@@ -0,0 +1,67 @@
+package com.example.android.miwok;
+
+/**
+ * Created by KUBRAMEVLUT on 3.04.2018.
+ */
+
+public class reportcard {
+    int mTurkceNot, mIngilizceNot, mMatNot, mFizikNot,mKimyaNot;
+
+    /**
+     * ReportCard objesi oluştur
+     *
+     * @param TurkceNot Turkish grade
+     *
+     * @param IngilizceNot English grade
+     *
+     * @param MatNot Math grade
+     *
+     * @param FizikNot  physics grade
+     *
+     * @param KimyaNot chemistry grade
+     */
+    public reportcard(int TurkceNot,int IngilizceNot, int MatNot,int FizikNot, int KimyaNot) {
+        mTurkceNot=TurkceNot;
+        mIngilizceNot=IngilizceNot;
+        mMatNot=MatNot;
+        mFizikNot=FizikNot;
+        mKimyaNot=KimyaNot;
+    }
+
+    /**
+     * Türkçe notunu getir
+     */
+    public int getmTurkceNot(){return mTurkceNot;}
+
+    /**
+     * Ingilizce notunu getir
+     */
+    public int getmIngilizceNot(){return mIngilizceNot;}
+
+    /**
+     * Matematik notunu getir
+     */
+    public int getmMatNot(){return mMatNot;}
+
+    /**
+     * Fizik notunu getir
+     */
+    public int getmFizikNot(){return mFizikNot;}
+
+    /**
+     * Kimya notunu getir
+     */
+    public int getmKimyaNot(){return mKimyaNot;}
+
+    public String toString() {
+        String mesaj;
+        mesaj="Name : Kubra\n";
+        mesaj +="Turkce notu =" + mTurkceNot;
+        mesaj+="\nIngilizce notu="+ mIngilizceNot;
+        mesaj+="\nMatematik notu="+ mMatNot;
+        mesaj+="\nFizik notu="+ mFizikNot;
+        mesaj+="\nKimya notu="+ mKimyaNot;
+
+        return mesaj;
+    }
+}
